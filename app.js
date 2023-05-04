f102b35a666104cdac0b3372e978989b8a9f338c
Dario Gutierrez Novoa <dario-yes@hotmail.com>
2023-05-04 13:45:13 -0500

    backEndConRutasOk

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // cargar archivos rutas
+var project_routes = require('./routes/project');
 
 // middlewares
 app.use(bodyParser.urlencoded({extended:false}));
@@ -14,19 +15,7 @@ app.use(bodyParser.json());
 // CORS
 
 // rutas
-
-app.get( '/',(req,res) =>{
-    res.status(200).send(
-        "<h1>Pagina de inicio</h1>"
-    );
-});
-
-app.get( '/test',(req,res) =>{
-    res.status(200).send(
-        {message: 'Hola mundo desde mi API de NodeJS'}
-    );
-});
-
+app.use('/api', project_routes);
 
 // exportar
 module.exports = app;
